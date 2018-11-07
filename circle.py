2fa2400ec309c51d363f1093ba42f9700c5be90b
Simon Streib <s.d.streib@tudelft.nl>
2018-11-07 14:44:29 +0100

    plots a circle

---
+++ circle.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-1, 1, 201)
+y1 = np.sqrt(1-x**2)
+y2 = - np.sqrt(1-x**2)
+
+plt.plot(x, y1, color='k')
+plt.plot(x, y2, color='k')
+
+plt.savefig('circle.png')
