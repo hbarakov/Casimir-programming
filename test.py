e5bb33330caa5ddaf6cf4caaae4c44335add706f
Hristo Barakov <hrbarakov@gmail.com>
2018-11-07 13:21:04 +0100

    Added a function that calculates the sruface area

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import numpy as np
-print('Hello World')
+
 def circle_circumference(r):
     return 2*np.pi*r
 
 print(circle_circumference(1))
+
+def circle_surface(r):
+    return np.pi*r**2
+
+print(circle_surface(1))
