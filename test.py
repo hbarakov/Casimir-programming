13a3558bb1a06845403754aac0404a79e4636329
Hristo Barakov <hrbarakov@gmail.com>
2018-11-07 13:44:30 +0100

    Added a description

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 def circle_circumference(r):
+    """A function that calculates the circumference of a cirle with radius r"""
     return 2*np.pi*r
 
 print(circle_circumference(1))
 
 def circle_surface(r):
+    """A function that calculates the surface area of a circle with radius r  """
     return np.pi*r**2
 
 print(circle_surface(1))
