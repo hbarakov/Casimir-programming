6593eefc1c7d5a36a38c9da8d922c923f60f9188
Simon Streib <s.d.streib@tudelft.nl>
2018-11-08 14:27:22 +0100

    letter and word counters

---
+++ counters.py
@@ -0,0 +1,15 @@
+import re
+from collections import Counter
+
+    
+def count_words(text_string):
+    text = re.sub('[^a-z]+', ' ', text_string.lower())
+    return Counter(text_string.split())
+    
+def count_letters(letter, text_string):
+    return text_string.count(letter, 0, len(text_string))
+
+
+#wordcount = count_word_occurences("a b c d e e e f  a")
+
+#for item in wordcount.most_common(10): print("{}\t{}".format(*item))
